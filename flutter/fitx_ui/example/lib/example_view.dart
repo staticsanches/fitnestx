ff8c8cbe5359025b0a90dc9d719e5f7c811f7ca0
Felipe Sanches <staticsanches@gmail.com>
2021-08-25 23:32:31 -0300

    Adding new buttons: primary, circle and switch

--- flutter/fitx_ui/example/lib/example_view.dart
+++ flutter/fitx_ui/example/lib/example_view.dart
@@ -1,14 +1,21 @@
 import 'package:fitx_ui/fitx_ui.dart';
 import 'package:flutter/material.dart';
 
-class ExampleView extends StatelessWidget {
+class ExampleView extends StatefulWidget {
   const ExampleView({Key? key}) : super(key: key);
+
+  @override
+  _ExampleViewState createState() => _ExampleViewState();
+}
+
+class _ExampleViewState extends State<ExampleView> {
+  bool _on = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 100),
+        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 100),
         children: [
           FitxText(
             'Danger',
@@ -26,9 +33,40 @@ class ExampleView extends StatelessWidget {
             gradient: FitxGradients.purpleLinear,
           ),
           SizedBox(height: 20),
-          FitxButton(gradient: FitxGradients.blueLinear),
+          FitxButton.primary(
+            "Login",
+            onPressed: () {},
+            leading: IconlyBold.login,
+          ),
           SizedBox(height: 20),
-          FitxButton(gradient: FitxGradients.purpleLinear),
+          FitxButton.primary(
+            "Next",
+            onPressed: () {},
+            trailing: IconlyLight.arrowRight2,
+          ),
+          SizedBox(height: 20),
+          FitxButton.primary("Button"),
+          SizedBox(height: 20),
+          FitxButton.purpleCircle(IconlyLight.camera),
+          SizedBox(height: 20),
+          FitxButton.purpleCircle(
+            IconlyLight.camera,
+            onPressed: () {},
+          ),
+          SizedBox(height: 20),
+          FitxButton.blueCircle(
+            Icons.add,
+            onPressed: () {},
+          ),
+          SizedBox(height: 20),
+          FitxButton.animatedSwitch(
+            _on,
+            onPressed: () {
+              setState(() {
+                _on = !_on;
+              });
+            },
+          ),
         ],
       ),
     );
--- flutter/fitx_ui/lib/fitx_ui.dart
+++ flutter/fitx_ui/lib/fitx_ui.dart
@@ -1,4 +1,7 @@
 library fitx_ui;
 
+// Icons
+export 'package:flutter_iconly/flutter_iconly.dart';
+
 export 'commons/commons.dart';
 export 'widgets/widgets.dart';
